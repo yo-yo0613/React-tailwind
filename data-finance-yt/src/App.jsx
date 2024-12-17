35c8d6bd149194aa5bbe73f6d492fc3bbaac2c85
unknown <Yoyo0613@>
2024-12-17 22:06:12 +0800

    First commit

---
+++ data-finance-yt/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
+import Analytics from "./Analytics.jsx";
+import Newsletter from "./Newsletter.jsx";
+import Cards from "./Cards.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Analytics></Analytics>
+      <Newsletter></Newsletter>
+      <Cards></Cards>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
